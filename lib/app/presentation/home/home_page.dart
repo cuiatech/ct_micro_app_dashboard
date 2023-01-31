44cd1eac414997df5912f7e8e57115d8c3d914fe
Raphael Pontes <raphael.pontes@condoconta.com.br>
2023-01-31 14:00:45 -0300

    help central

--- lib/app/presentation/home/home_page.dart
+++ lib/app/presentation/home/home_page.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flut_micro_commons_ds/flut_micro_commons_ds.dart';
+import 'package:flut_micro_app_help_center/app/presentation/help_center_widget/help_center_widget.dart';
 
 import 'widgets/body_list.dart';
 
@@ -18,7 +19,7 @@ class HomePage extends StatelessWidget {
       endDrawer: const Drawer(
         backgroundColor: Colors.white,
         width: 500,
-        child: SizedBox(),
+        child: HelpCenterWidget(),
       ),
     );
   }

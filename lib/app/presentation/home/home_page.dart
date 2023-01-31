06a744ce5c4c3a2d2ca473717c11143fc011dd3b
Raphael Pontes <raphael.pontes@condoconta.com.br>
2023-01-31 01:40:18 -0300

    upd

--- lib/app/presentation/home/home_page.dart
+++ lib/app/presentation/home/home_page.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: depend_on_referenced_packages
 
 import 'package:flutter/material.dart';
-import 'package:flut_micro_commons_shared/flut_micro_commons_shared.dart';
+import 'package:flut_micro_commons_ds/flut_micro_commons_ds.dart';
 
 import 'widgets/body_list.dart';
 
@@ -22,52 +22,4 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
-}
-
-class CuiaAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const CuiaAppBar({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 80,
-      color: const Color(0xff2196f3),
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          InkWell(
-            onTap: () {},
-            child: CuiaLogo.horizontal(
-              text: "Workspace",
-              width: 50,
-              fontSize: 18,
-            ),
-          ),
-          Row(
-            children: [
-              InkWell(
-                onTap: () {
-                  Scaffold.of(context).openEndDrawer();
-                },
-                child: CuiaIcons.support(white: true, size: 28),
-              ),
-              const SizedBox(width: 20),
-              InkWell(
-                onTap: () {},
-                child: const CircleAvatar(
-                  child: Text("RP"),
-                ),
-              ),
-            ],
-          )
-        ],
-      ),
-    );
-  }
-
-  @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }

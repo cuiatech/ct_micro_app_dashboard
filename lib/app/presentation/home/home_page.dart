2312058b2e16c5455e397eca7674a306cc5df5fa
Raphael Pontes <raphael.pontes@condoconta.com.br>
2023-02-01 23:58:23 -0300

    upd

--- lib/app/presentation/home/home_page.dart
+++ lib/app/presentation/home/home_page.dart
@@ -1,10 +1,9 @@
 // ignore_for_file: depend_on_referenced_packages
 
+import 'package:flut_micro_app_dashboard/app/presentation/home/widgets/body_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flut_micro_commons_ds/flut_micro_commons_ds.dart';
 import 'package:flut_micro_app_help_center/app/presentation/help_center_widget/help_center_widget.dart';
-
-import 'widgets/body_list.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -12,10 +11,24 @@ class HomePage extends StatelessWidget {
   @override
   @override
   Widget build(BuildContext context) {
+    var apps = [
+      {
+        "title": "Syncro",
+        "description": "Síncronismo de produtos nos principais e-commerces",
+      },
+      {
+        "title": "eSaller",
+        "description": "Gerenciamento de vitrine online",
+      },
+      {
+        "title": "myDelivery",
+        "description": "Gerencie pedidos, entregadores e cozinha.",
+      },
+    ];
     return Scaffold(
       key: key,
       appBar: const CuiaAppBar(),
-      body: const DashboardBodyList(),
+      body: DashboardBodyList(apps: apps),
       endDrawer: const Drawer(
         backgroundColor: Colors.white,
         width: 500,
--- lib/app/presentation/home/widgets/body_list.dart
+++ lib/app/presentation/home/widgets/body_list.dart
@@ -6,63 +6,76 @@ import 'package:flut_micro_commons_ds/flut_micro_commons_ds.dart';
 class DashboardBodyList extends StatelessWidget {
   const DashboardBodyList({
     Key? key,
+    required this.apps,
   }) : super(key: key);
+
+  final List apps;
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: CuiaGridRow(
-        children: List.generate(
-          10,
-          (index) => CuiaGridColumn(
-            xs: 6,
-            sm: 5,
-            md: 4,
-            lg: 3,
-            xl: 2,
-            child: Container(
-              padding: const EdgeInsets.all(10),
-              margin: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(
-                  color: const Color(0xff2196f3),
-                  width: 1,
-                ),
-                color: Colors.white,
-              ),
-              child: InkWell(
-                onTap: () {},
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Image.asset(
+        children: apps
+            .map((e) => _item(title: e["title"], description: e["description"]))
+            .toList(),
+      ),
+    );
+  }
+
+  CuiaGridColumn _item({
+    required String title,
+    required String description,
+    String? urlImage,
+  }) {
+    return CuiaGridColumn(
+      xs: 6,
+      sm: 5,
+      md: 4,
+      lg: 3,
+      xl: 2,
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        margin: const EdgeInsets.all(10),
+        height: 250,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+            color: const Color(0xff2196f3),
+            width: 1,
+          ),
+          color: Colors.white,
+        ),
+        child: InkWell(
+          onTap: () {},
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              urlImage == null
+                  ? Image.asset(
                       '/assets/images/system_page.png',
                       package: 'flut_micro_commons_shared',
-                    ),
-                    const SizedBox(height: 8),
-                    const Text(
-                      "Syncro",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 22,
-                      ),
-                    ),
-                    const SizedBox(height: 8),
-                    const Text(
-                      "Síncronismo de produtos nos e-commerces",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Color(0xff8f8f8f),
-                        fontSize: 14,
-                      ),
-                    ),
-                  ],
+                    )
+                  : Image.network(urlImage),
+              const SizedBox(height: 8),
+              Text(
+                title,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 22,
                 ),
               ),
-            ),
+              const SizedBox(height: 8),
+              Text(
+                description,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  color: Color(0xff8f8f8f),
+                  fontSize: 14,
+                ),
+              ),
+            ],
           ),
         ),
       ),
